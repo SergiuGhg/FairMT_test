4a1e1b15e7dca873b32afa6896f5ecb340d91445
SergiuGhg <149172867+SergiuGhg@users.noreply.github.com>
2026-04-02 22:56:23 +0300

    Update all_answers.py

--- FairMT-bench-main/all_answers.py
+++ FairMT-bench-main/all_answers.py
@@ -1,7 +1,7 @@
 import os
 import subprocess
 
-BASE = "../FairMT_10K"
+BASE = "FairMT_10K"
 MODEL = "meta-llama/Meta-Llama-3.1-8B-Instruct"
 PROMPT = "code/prompt_standard.txt"
 SAVE = "../outputs"
