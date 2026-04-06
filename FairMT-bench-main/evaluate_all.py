17821ef3655585dabbbe7e51681e60d71e11dc7c
SergiuGhg <149172867+SergiuGhg@users.noreply.github.com>
2026-04-06 23:10:06 +0300

    Add files via upload

---
+++ FairMT-bench-main/evaluate_all.py
@@ -0,0 +1,25 @@
+import os
+import subprocess
+
+BASE = "../../outputs"
+MODEL = "meta-llama/Meta-Llama-Guard-3-8B"
+PROMPT = "code/prompt_standard.txt"
+SAVE = "../evals"
+
+for root, dirs, files in os.walk(BASE):
+    for file in files:
+        if file.endswith(".json"):
+            dataset_path = os.path.join(root, file)
+
+            print(f"\n=== Running on {dataset_path} ===\n")
+
+            cmd = [
+                "python", "evaluation_coreference.py",
+                "--model", MODEL,
+                "--prompt", PROMPT,
+                "--dataset", dataset_path,
+                "--save_path", SAVE,
+                "--num_samples", "-1"
+            ]
+
+            subprocess.run(cmd)
