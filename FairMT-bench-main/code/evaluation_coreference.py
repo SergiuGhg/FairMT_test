1a87670785942f04aeaf9862aeec0fd5e698a61c
SergiuGhg <149172867+SergiuGhg@users.noreply.github.com>
2026-04-07 00:51:35 +0300

    Fix formatting of prompt assignment in evaluation_coreference.py

--- FairMT-bench-main/code/evaluation_coreference.py
+++ FairMT-bench-main/code/evaluation_coreference.py
@@ -121,11 +121,11 @@ def chat_completion_llama3(system, prompt):
         {"role": "user", "content": prompt},
     ]
 
-    prompt = tokenizer.apply_chat_template(
+    """prompt = tokenizer.apply_chat_template(
             messages, 
             tokenize=False, 
             add_generation_prompt=True
-    )
+    )"""
 
     terminators = [
         pipeline.tokenizer.eos_token_id,
