5434f63b7725be6d96340ec70fc10de826a78947
SergiuGhg <149172867+SergiuGhg@users.noreply.github.com>
2026-04-28 14:25:10 +0300

    Update all_answers.py

--- FairMT-bench-main/all_answers.py
+++ FairMT-bench-main/all_answers.py
@@ -4,7 +4,7 @@ import subprocess
 BASE = "FairMT_10K"
 MODEL = "meta-llama/Meta-Llama-3.1-8B-Instruct"
 PROMPT = "code/prompt_standard.txt"
-SAVE = "../outputs"
+SAVE = "../outputs_gender_plus_1"
 
 for root, dirs, files in os.walk(BASE):
     for file in files:
@@ -14,7 +14,7 @@ for root, dirs, files in os.walk(BASE):
             print(f"\n=== Running on {dataset_path} ===\n")
 
             cmd = [
-                "python", "generate_answer.py",
+                "python", "generate_answer_vectors.py",
                 "--model", MODEL,
                 "--prompt", PROMPT,
                 "--dataset", dataset_path,
